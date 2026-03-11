52575323ca62794eab932e3fed6ec4004cf7bd09
Ashurov <roman.ash.dm@gmail.com>
2026-03-11 21:47:34 +0300

    wip_search

--- src/python_kt_1/use_cases/search.py
+++ src/python_kt_1/use_cases/search.py
@@ -6,7 +6,7 @@ from ..core.types import SearchResult
 
 
 def search(
-    pattern: str, file_path: pathlib.Path, is_regex: bool = False
+        pattern: str, file_path: pathlib.Path, is_regex: bool = False
 ) -> typing.Iterable[SearchResult]:
     """Поиск подстроки в текстовом файле
 
@@ -30,4 +30,33 @@ def search(
 
     text = file_path.read_text(encoding="utf-8")
 
-    return []
+    if is_regex:
+        return regex_search(pattern, text)
+    return normal_search(pattern, text)
+
+
+def regex_search(pattern: str, text: str) -> list[SearchResult]:
+    try:
+        return [
+            {"result": match.group(), "start": match.start(), "end": match.end()}
+            for match in re.finditer(pattern, text)
+        ]
+    except re.error:
+        raise re.error("Invalid regular expression")
+
+
+def normal_search(pattern: str, text: str) -> list[SearchResult]:
+    results = []
+    start = 0
+    while True:
+        pos = text.find(pattern, start)
+        if pos == -1:
+            break
+
+        results.append({
+            "result": pattern,
+            "start": pos,
+            "end": pos + len(pattern) - 1,
+        })
+        start = pos + 1
+    return results
